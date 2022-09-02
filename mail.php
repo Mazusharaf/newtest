a2b3bc87de2a43073f662ad681c0cddd65dcb97b
Mazusharaf <108242965+Mazusharaf@users.noreply.github.com>
2022-09-03 01:50:08 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "mashud8168@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: noreply@mazusharaf.github.io/newtest/" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
